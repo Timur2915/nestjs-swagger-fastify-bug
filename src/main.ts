fe90b278bb9e559bb6fd1073b6bd164c5cee55e2
Timur Popov <timur2915@gmail.com>
2019-03-21 15:06:06 +0300

    Migrate to FastifyAdapter, add SwaggerModule loading

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  const basePath = 'api';
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('API description')
+    .setBasePath(basePath)
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`${basePath}/docs`, app, document);
+
   await app.listen(3000);
 }
 bootstrap();
